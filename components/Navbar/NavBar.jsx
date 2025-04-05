b86fb4931008d0c8f7aeece9fa208d5732f5e894
Caiko <marinenikolov@gmail.com>
2025-04-05 22:44:19 +0200

    Fixed nav background color

--- components/Navbar/NavBar.jsx
+++ components/Navbar/NavBar.jsx
@@ -2,10 +2,33 @@
 import styles from "./NavBar.module.scss";
 import ScrambleButton from "../ScrambleButton/ScrambleButton";
 
+import { useEffect, useState } from "react";
+
 export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <header>
-      <nav aria-label="Main Navigation" className={styles.nav}>
+      <nav
+        aria-label="Main Navigation"
+        className={`${styles.nav} ${isScrolled ? styles.scrolled : ""} `}
+      >
         <ul className={styles.pages}>
           <li>
             <ScrambleButton
