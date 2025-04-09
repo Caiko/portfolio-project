779c6628d21eb9200e710c75170c4f9a28a3e825
Caiko <marinenikolov@gmail.com>
2025-04-09 16:17:05 +0200

    Icons finished

--- components/Icons/Icons.jsx
+++ components/Icons/Icons.jsx
@@ -3,18 +3,38 @@ import styles from "./Icons.module.scss";
 export default function Icons() {
   return (
     <div className={styles.container}>
-      <svg className={styles.icon}>
-        <use href="/icons/sprite.svg#icon-github"></use>
-      </svg>
-      <svg className={styles.icon}>
-        <use href="/icons/sprite.svg#icon-linkedin"></use>
-      </svg>
-      <svg className={styles.icon}>
-        <use href="/icons/sprite.svg#icon-instagram"></use>
-      </svg>
-      <svg className={styles.icon}>
-        <use href="/icons/sprite.svg#icon-mail"></use>
-      </svg>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/Caiko"
+      >
+        <svg className={styles.icon}>
+          <use href="/icons/sprite.svg#icon-github"></use>
+        </svg>
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.linkedin.com/in/marin-nikolov-22b64b163/"
+      >
+        <svg className={styles.icon}>
+          <use href="/icons/sprite.svg#icon-linkedin"></use>
+        </svg>
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.instagram.com/marinenikolov/"
+      >
+        <svg className={styles.icon}>
+          <use href="/icons/sprite.svg#icon-instagram"></use>
+        </svg>
+      </a>
+      <a href="mailto:marinenikolov@gmail.com" aria-label="Send me an email">
+        <svg className={styles.icon}>
+          <use href="/icons/sprite.svg#icon-mail"></use>
+        </svg>
+      </a>
     </div>
   );
 }
