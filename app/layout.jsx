cd37d953a305241c65d04189808149ae58b799d8
Caiko <marinenikolov@gmail.com>
2025-04-26 19:44:33 +0200

    metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,14 +4,73 @@ import "@/styles/globals.scss";
 
 export const metadata = {
   title: "Marin Nikolov",
-  description: "Built with Next.js 15",
+  description:
+    "Showcasing Dimitar Terziev's projects, skills, and experience in software development.",
+  keywords: [
+    "Marin Nikolov",
+    "Portfolio",
+    "Software Developer",
+    "Web Development",
+    "Frontend Developer",
+    "Backend Developer",
+    "React",
+    "Next.js",
+    "JavaScript",
+    "TypeScript",
+    "Digital Transformation",
+    "Business Strategy",
+    "Supply Chain Optimization",
+    "Process Automation",
+    "SAP ERP",
+    "Power BI",
+    "Excel",
+    "Notion",
+    "Jira",
+    "SSMS",
+    "C#",
+    "Node.js",
+    "Express.js",
+    "HTML",
+    "CSS",
+    "SASS",
+    "SCSS",
+    "NestJS",
+    ".NET",
+    "SQL",
+    "Tailwind CSS",
+    "GitHub",
+    "Docker",
+    "PostgreSQL",
+    "MongoDB",
+    "Vite",
+    "Visual Studio Code",
+    "Cloud Computing",
+    "VPNs",
+    "Cybersecurity",
+    "Server Management",
+    "Linux Administration",
+    "Networking Tools",
+    "Terraform",
+    "Ansible",
+    "pfSense",
+    "Proxmox",
+    "WireGuard",
+    "Tailscale",
+  ],
+  authors: [{ name: "Marin Nikolov", url: "https://marinnikolov.com/" }],
+  creator: "Marin Nikolov",
+  publisher: "Marin Nikolov",
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>My Next.js App</title>
+        <title>Marin Nikolov</title>
       </head>
       <body>
         <Navbar />
